3d3558df2123c256dff9cb764cb51d27a0071f25
Alexander Arif <alex.arif@mechanicalrock.io>
2019-06-24 10:02:34 +0800

    ignoring secrets

---
+++ secrets.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+password=P@ssw0rd123! # pragma: allowlist secret
+secret=MySecret123@! # pragma: allowlist secret
+
+AWS_KEY="AKIAY5XAIXXR7ALTOOHA" # pragma: allowlist secret
+AWS_SECRET_ACCESS_KEY="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" # pragma: allowlist secret
