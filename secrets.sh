7742b0cb8b9703b19e86285d130e0879e27d1d8d
Alexander Arif <alex.arif@mechanicalrock.io>
2019-06-24 10:51:52 +0800

    testing client_secret

--- secrets.sh
+++ secrets.sh
@@ -2,5 +2,4 @@
 password=P@ssw0rd123! # pragma: allowlist secret
 secret=MySecret123@! # pragma: allowlist secret
 
-export AWS_KEY="AKIAY5XAIXXR7ALTOOHB" # pragma: allowlist secret
-export AWS_SECRET_ACCESS_KEY="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" # pragma: allowlist secret
+client_secret="czZCaGRSa3F0MzpnWDFmQmF0M2JW" # pragma: allowlist secret
