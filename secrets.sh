26b0a7292167590088a65844133afe99d6c10b84
Alexander Arif <alex.arif@mechanicalrock.io>
2019-06-24 10:47:39 +0800

    update dummy values, testing gitguardian

--- secrets.sh
+++ secrets.sh
@@ -2,5 +2,5 @@
 password=P@ssw0rd123! # pragma: allowlist secret
 secret=MySecret123@! # pragma: allowlist secret
 
-AWS_KEY="AKIAY5XAIXXR7ALTOOHA" # pragma: allowlist secret
-AWS_SECRET_ACCESS_KEY="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" # pragma: allowlist secret
+export AWS_KEY="AKIAY5XAIXXR7ALTOOHB" # pragma: allowlist secret
+export AWS_SECRET_ACCESS_KEY="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" # pragma: allowlist secret
