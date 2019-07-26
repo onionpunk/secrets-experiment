1d190282f3ad12d11f95bb10ea06f0b92653dbac
Alexander Arif <alex.arif@mechanicalrock.io>
2019-07-26 15:37:21 +0800

    test something else

--- secrets.sh
+++ secrets.sh
@@ -5,6 +5,7 @@ secret=MySecret123@! # pragma: allowlist secret
 myvariable="czZCaGRSa3F0MzpnWDFmQmF0M2JWX"
 
 AWS_KEY="AKIAY5XAIXXR7ALTOOHH"
+AWS_KEY2="AKIAY5XAIXXR7ALTOOHH"
 myvariable_access="3L091LfFvV/dcoDCy5YO7sWz56bitLQjlVVY2WxmY"
 
 fake_secret_sendgrid=SG.af7c6bMLRxSagExMsW8Hng.vaa_FWbgCcVdkXhDXXg23SWS4cR5RcRtClTcdIb2d3b
