ad5ddfa1b200c5543159d5aaafae1589048a2b38
Alexander Arif <alex.arif@mechanicalrock.io>
2019-07-26 13:16:25 +0800

    test

--- secrets.sh
+++ secrets.sh
@@ -2,7 +2,7 @@
 password=P@ssw0rd123! # pragma: allowlist secret
 secret=MySecret123@! # pragma: allowlist secret
 
-client_secret="czZCaGRSa3F0MzpnWDFmQmF0M2JWX" # pragma: allowlist secret
+myvariable="czZCaGRSa3F0MzpnWDFmQmF0M2JWX"
 
 AWS_KEY="AKIAY5XAIXXR7ALTOOHC"
-AWS_SECRET_ACCESS_KEY="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"
+myvariable_access="3L091LfFvV/dcoDCy5YO7sWz56bitLQjlVVY2ZxmY"
