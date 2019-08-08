83c8fe9cd36d8fc85f2e6e8378b6f0c6b2547efc
Alexander Arif <alex.arif@mechanicalrock.io>
2019-08-08 13:33:12 +0800

    test

--- readme.md
+++ readme.md
@@ -1,8 +1,1 @@
-first line
-
-second line
-
-third line
-
-AWS_KEY="AKIAY5XAIXXR7ALTOOHC"
-AWS_KEY_SECRET="3L091LfFvV/dcoDCy5YO7sWz56bitLQjlVVY2ZxmY"
+testing jfrog xray
--- secrets.sh
+++ secrets.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-password=P@ssw0rd123! # pragma: allowlist secret
-secret=MySecret123@! # pragma: allowlist secret
-
-myvariable="czZCaGRSa3F0MzpnWDFmQmF0M2JWX"
-
-AWS_KEY="AKIAY5XAIXXR7ALTOOHH"
-AWS_KEY2="AKIAY5XAIXXR7ALTOOHH"
-myvariable_access="3L091LfFvV/dcoDCy5YO7sWz56bitLQjlVVY2WxmY"
-
-fake_secret_sendgrid=SG.af7c6bMLRxSagExMsW8Hng.vaa_FWbgCcVdkXhDXXg23SWS4cR5RcRtClTcdIb2d3b
+MY_KEY="AKIAY5XAIXXR7ALTOOHH"
+MY_SECRET="3L091LfFvV/dcoDCy5YO7sWz56bitLQjlVVY2WxmY"
